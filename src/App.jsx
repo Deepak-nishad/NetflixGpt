c8930dfe80dc00bfb98cdb8eabd1b7aaf3b8e0e6
Deepak-nishad <deepakkumar62559@gmail.com>
2025-02-20 12:29:08 +0530

    Components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+Login
+import Body from './components/Body'
+import Browser from './components/Browser'
+import Login from './components/Login'
+Browser
+function App() {
+
+
+  return (
+    <>
+      <Login />
+      <Browser />
+    </>
+  )
+}
+
+export default App
